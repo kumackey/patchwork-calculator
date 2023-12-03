43def5e1a35956d284a66d85186875dbbfc89fdd
kumackey <kumak1t09e0@gmail.com>
2023-12-03 11:17:56 +0900

    Adding an Amazon Associates iframe

--- src/App.tsx
+++ src/App.tsx
@@ -29,17 +29,34 @@ function App() {
             <h1>Placed Patches</h1>
             <PlacedPatchList placedPatches={placedPatches}/>
 
-            <h1>Formula</h1>
-            <p>total cost = (patch size) * 2 + (buttons earned) * (remaining income times)</p>
-            <p>button rate = (total cost) / ((button cost) + (time cost))</p>
-            <p>time rate = ((total cost) - (button cost)) / (time cost)</p>
-            <p>
-                For more details, see the <a
-                href="https://boardgamegeek.com/thread/1307009/patchwork-tactic-maximizing-buttons-time"
-                target="_blank">A Patchwork Tactic: Maximizing Buttons Per Time</a>.
-            </p>
+            <hr/>
+            <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center'}}>
+                <div style={{flex: 1, minWidth: '300px', padding: '10px'}}>
+                    <h1>Formula</h1>
+                    <p>total cost = (patch size) * 2 + (buttons) * (remaining income times)</p>
+                    <p>button rate = (total cost) / ((button cost) + (time cost))</p>
+                    <p>time rate = ((total cost) - (button cost)) / (time cost)</p>
+                    <p>
+                        For more details, see <a
+                        href="https://boardgamegeek.com/thread/1307009/patchwork-tactic-maximizing-buttons-per-time"
+                        target="_blank">A Patchwork Tactic: Maximizing Buttons Per Time</a>.
+                    </p>
+                    <p>Contact: <a href="https://twitter.com/kumackey_" target="_blank">@kumackey_</a></p>
+                </div>
 
-            <p>Contact: <a href="https://twitter.com/kumackey_" target="_blank">@kumackey_</a></p>
+                <div style={{flex: 1, minWidth: '120px', padding: '10px'}}>
+                    <iframe
+                        sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
+                        style={{width: '120px', height: '240px'}}
+                        marginWidth={0}
+                        marginHeight={0}
+                        scrolling="no"
+                        frameBorder="0"
+                        src="//rcm-fe.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=kumackey06-22&language=ja_JP&o=9&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B00RCCAPPE&linkId=6a14e40ff16fa38ee3e0eccf25205401">
+                    </iframe>
+                </div>
+            </div>
+
         </div>
     );
 }
